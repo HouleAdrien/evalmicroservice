090f6c48c6b64be162ce5fc48c0052d93ef5f8cc
KorentinB <korentin.bertevas@example.com>
2021-12-07 10:14:27 +0100

    Add Repository and Controller

--- service-auteur/src/main/java/com/ynov/b3info/eval/repository/AuteurRepository.java
+++ service-auteur/src/main/java/com/ynov/b3info/eval/repository/AuteurRepository.java
@@ -1,5 +1,8 @@
 package com.ynov.b3info.eval.repository;
 
-public class AuteurRepository {
+import org.springframework.data.repository.CrudRepository;
+import com.ynov.b3info.eval.models.Auteur;
 
+public interface AuteurRepository extends CrudRepository<Auteur, Integer> {
+	public Iterable<Auteur> findByNameStartingWith(String name);
 }
